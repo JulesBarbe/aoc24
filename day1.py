5f91481c58f971e9d03eff2d276c3dd573a190fa
Jules Barbe <ajules32@gmail..com>
2024-12-03 15:27:42 -0500

    refactor day1

--- day1.py
+++ day1.py
@@ -1,21 +1,20 @@
-with open("day1.txt", 'r') as lists:
-    # x = lists.read().splitlines()
-    # l1, l2 = [], []
-    # for line in x:
-    #     line = line.split()
-    #     l1.append(int(line[0]))
-    #     l2.append(int(line[1]))
-    # l1.sort()
-    # l2.sort()
-    # r = sum([abs(x1 - x2) for (x1, x2) in zip(l1, l2)])
-    # print(r)
-    lines = lists.read().splitlines()
+def problem1(lines):
+    l1, l2 = [], []
+    for line in lines:
+        line = line.split()
+        l1.append(int(line[0]))
+        l2.append(int(line[1]))
+    l1.sort()
+    l2.sort()
+    r = sum([abs(x1 - x2) for (x1, x2) in zip(l1, l2)])
+    print(r)
+
+def problem2(lines): 
     d = {}
     s = set()
     for line in lines:
         line = line.split()
         l, r = int(line[0]), int(line[1])
-        print(l, r)
         s.add(l)
         d[r] = d.get(r, 0) + 1
             
@@ -23,4 +22,8 @@ with open("day1.txt", 'r') as lists:
 
 
 
-  
+if __name__ == "__main__":
+    with open("day1.txt", 'r') as file:
+        lines = file.read().splitlines()
+        problem1(lines)
+        problem2(lines) 
